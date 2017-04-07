1bc5ef454fc8b8c2243f178b4a55e440b07321eb
luisrodge <luisrodge@outlook.com>
2017-04-06 23:43:44 -0600

    style

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
   root 'tournaments#index'
-  
-  devise_for :users
+
+  devise_for :users, skip: [:sessions]
+  as :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 
   resources :teams
   resources :venues, only: :index
@@ -16,7 +21,7 @@ Rails.application.routes.draw do
   namespace :profile do
     resources :venues
   	resources :teams do
-      member do 
+      member do
         put "join"
         delete "leave"
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402031316) do
+ActiveRecord::Schema.define(version: 20170407050250) do
 
   create_table "challenges", force: :cascade do |t|
     t.integer  "team_id"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20170402031316) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "profile_image"
+    t.string   "full_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
