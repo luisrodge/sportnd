2c1ccd7fbd4c22ec363cee4e1f74313d00fcec46
luisrodge <luisrodge@outlook.com>
2017-05-05 16:31:36 -0600

    migration alter

--- db/migrate/20170329181749_create_enrollments.rb
+++ db/migrate/20170329181749_create_enrollments.rb
@@ -1,8 +1,8 @@
 class CreateEnrollments < ActiveRecord::Migration[5.0]
   def change
     create_table :enrollments do |t|
-      t.references :team, foreign_key: true
-      t.references :tournament, foreign_key: true
+      t.integer :team, foreign_key: true
+      t.integer :tournament, foreign_key: true
       t.boolean :organizer
 
       t.timestamps
