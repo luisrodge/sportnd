6703fa459baf611ccf87a5e215e62c19996d6e68
luisrodge <luisrodge@outlook.com>
2017-05-06 20:53:11 -0600

    style change

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -1,9 +1,5 @@
 module TournamentsHelper
 
-  def new_tournament_btn
-    link_to "Start New Tournament", new_profile_tournament_path, class: "btn btn-primary round"
-  end
-
   def teams_enrollment_status(tournament)
     if tournament.teams.count == tournament.capacity
       if tournament.teams_with_space > 0
