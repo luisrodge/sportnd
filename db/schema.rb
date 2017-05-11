b0f2b386d94e17a67364be788e08579b2c3c2c53
luisrodge <luisrodge@outlook.com>
2017-05-10 21:16:45 -0600

    allowed null email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510220537) do
+ActiveRecord::Schema.define(version: 20170511031503) do
 
   create_table "challenges", force: :cascade do |t|
     t.integer  "team_id"
@@ -100,7 +100,6 @@ ActiveRecord::Schema.define(version: 20170510220537) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -119,7 +118,7 @@ ActiveRecord::Schema.define(version: 20170510220537) do
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
     t.string   "provider"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.string   "email",                  default: ""
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
